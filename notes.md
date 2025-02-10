a578e34c0cdaa4f7262f7ba8717d61fa5c790962
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-02-10 09:22:23 +0700

    demo Button Group

--- notes.md
+++ notes.md
@@ -1,10 +1,21 @@
-1. Typography
-   <Typographyvariant="h1"component="h2"> h1. Heading</Typography>
-   - Văn bản sẽ hiển thị với kiểu dáng, fontsize, font-weight, margin của h1
-   - Nhưng trong cấu trúc html, nó sẽ là một thẻ h2 chứ không phải là h1
-     Điều này hữu ích khi muốn giữ phong cách thiết kế nhưng vẫn đảm bảo cấu trúc SEO hợp lý
-2. Button
+# 1. Typography
+
+<Typographyvariant="h1"component="h2"> h1. Heading</Typography>
+
+- Văn bản sẽ hiển thị với kiểu dáng, fontsize, font-weight, margin của h1
+- Nhưng trong cấu trúc html, nó sẽ là một thẻ h2 chứ không phải là h1
+  Điều này hữu ích khi muốn giữ phong cách thiết kế nhưng vẫn đảm bảo cấu trúc SEO hợp lý
+
+# 2. Button
 
 - props disableElevation dùng để tắt hiệu ứng bóng đổ khi click vào button
 
 - disableTouchRipple dùng để tát hiệu ứng lan tỏa khi click vào button
+
+# 3.Button Group
+
+- Là một component dùng để nhóm nhiều Button lại với nhau tạo thành có bố cục và giao diện thống nhất
+- Công dụng chính
+  - Gộp nhiều nút lại thành một nhóm liên kết với nhau
+  - Hỗ trợ bố cục ngang dọc
+  - Làm cho các nút trong cùng một group có cùng variant, size, color
--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,69 +1,90 @@
-import { Button, Stack, IconButton } from "@mui/material";
+import { Button, Stack, IconButton, ButtonGroup } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import SendIcon from "@mui/icons-material/Send";
 const MuiButton = () => {
   return (
-    <Stack direction="row" spacing={5}>
-      <Button
-        variant="text"
-        size="small"
-        color="info"
-        sx={{ backgroundColor: "#000" }}
-      >
-        Text
-      </Button>
-      <Button
-        variant="contained"
-        size="medium"
-        color="success"
-        startIcon={<DeleteIcon />}
-      >
-        Contained size medium
-      </Button>
-      <Button
-        variant="outlined"
-        size="large"
-        color="warning"
-        endIcon={<SendIcon />}
-      >
-        Outlined size large
-      </Button>
-      <IconButton
-        color="error"
-        aria-label="delete"
-        sx={{
-          "&:hover": { backgroundColor: "transparent" },
-          width: 100,
-          height: 100,
-        }}
-      >
-        <DeleteIcon />
-      </IconButton>
-      <Button
-        variant="contained"
-        size="medium"
-        color="success"
-        disableTouchRipple
-        onClick={() => alert("Success")}
-      >
-        Success
-      </Button>
-      <Button variant="contained" size="medium" color="error">
-        Error
-      </Button>
-      <Button variant="contained" size="medium" color="info">
-        Info
-      </Button>
-      <Button variant="contained" size="medium" color="warning">
-        Warning
-      </Button>
-      <Button variant="contained" size="medium" color="primary">
-        Primary
-      </Button>
-      <Button variant="contained" size="medium" color="secondary">
-        Secondary
-      </Button>
-    </Stack>
+    <>
+      <Stack direction="row" spacing={5}>
+        <Button
+          variant="text"
+          size="small"
+          color="info"
+          sx={{ backgroundColor: "#000" }}
+        >
+          Text
+        </Button>
+        <Button
+          variant="contained"
+          size="medium"
+          color="success"
+          startIcon={<DeleteIcon />}
+        >
+          Contained size medium
+        </Button>
+        <Button
+          variant="outlined"
+          size="large"
+          color="warning"
+          endIcon={<SendIcon />}
+        >
+          Outlined size large
+        </Button>
+        <IconButton
+          color="error"
+          aria-label="delete"
+          sx={{
+            "&:hover": { backgroundColor: "transparent" },
+            width: 100,
+            height: 100,
+          }}
+        >
+          <DeleteIcon />
+        </IconButton>
+        <Button
+          variant="contained"
+          size="medium"
+          color="success"
+          disableTouchRipple
+          onClick={() => alert("Success")}
+        >
+          Success
+        </Button>
+        <Button variant="contained" size="medium" color="error">
+          Error
+        </Button>
+        <Button variant="contained" size="medium" color="info">
+          Info
+        </Button>
+        <Button variant="contained" size="medium" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" size="medium" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" size="medium" color="secondary">
+          Secondary
+        </Button>
+      </Stack>
+      <Stack direction="row" sx={{ marginTop: 2 }}>
+        <ButtonGroup
+          variant="contained"
+          orientation="vertical"
+          size="large"
+          color="secondary"
+          aria-label="alignment button group"
+        >
+          <Button
+            onClick={() => {
+              alert("Demo Button Group");
+            }}
+          >
+            Left
+          </Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+    </>
   );
 };
 
